2f021790eb236f5a57370ccfd0f91f76319ec6eb
Lahouaria1 <yahiaboya37@gmail.com>
2025-04-02 21:20:03 +0200

    Initial commit

--- salonbooking/generateSecret.js
+++
@@ -1,8 +0,0 @@
-// generateSecret.js
-const crypto = require('crypto');
-
-// Generate a 64-byte random string and convert it to hex
-const secret = crypto.randomBytes(64).toString('hex');
-
-// Print the generated secret
-console.log(secret);
